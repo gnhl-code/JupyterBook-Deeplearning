f882c8cbaf177ed1654b64b3d93c3287c3b6b514
gnhyl-code <felisilda.genheylou@gmail.com>
2025-11-21 22:29:51 +0800

    updated

--- MyJupyterBook/conf.py
+++ MyJupyterBook/conf.py
@@ -6,7 +6,7 @@
 author = 'Genheylou Felisilda'
 bibtex_bibfiles = ['references.bib']
 comments_config = {'hypothesis': False, 'utterances': False}
-copyright = '2023'
+copyright = '2025, Genheylou Felisilda'
 exclude_patterns = ['**.ipynb_checkpoints', '.DS_Store', 'Thumbs.db', '_build']
 extensions = ['sphinx_togglebutton', 'sphinx_copybutton', 'myst_nb', 'jupyter_book', 'sphinx_thebe', 'sphinx_comments', 'sphinx_external_toc', 'sphinx.ext.intersphinx', 'sphinx_design', 'sphinx_book_theme', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode', 'sphinxcontrib.bibtex', 'sphinx_jupyterbook_latex', 'sphinx_multitoc_numbering']
 external_toc_exclude_missing = False
